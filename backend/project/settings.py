b9fdfbd4cf59fe36348b45cfd2ce51c4ca8b1f50
Josefa <josefa.aravena@inacapmail.cl>
2026-06-15 16:27:00 -0400

    settings ip telefono

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -156,6 +156,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://192.168.56.1:5173',
     'http://192.168.100.29:5173',
+    'http://192.168.1.152:5173/',
     'https://server-production-36cb.up.railway.app',
     'https://elebann.github.io'
 ]
@@ -168,6 +169,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://192.168.56.1:5173',
     'http://192.168.100.29:5173',
+    'http://192.168.1.152:5173/',
     'https://server-production-36cb.up.railway.app',
     'https://elebann.github.io'
 ]
